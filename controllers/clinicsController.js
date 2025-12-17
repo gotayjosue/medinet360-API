52b72b8ebae88ae3c825f0fffe9665fffdd051fc
gotayjosue <gotayjosue@gmail.com>
2025-12-16 21:20:26 -0600

    Clinic controller fixed

--- controllers/clinicsController.js
+++ controllers/clinicsController.js
@@ -3,6 +3,9 @@ const Clinic = require("../models/Clinic.js");
 const getClinicById = async (req, res) => {
   try {
     const clinic = await Clinic.findById(req.params.id);
+    if (!clinic) {
+      return res.status(404).json({ error: "Clinic not found" });
+    }
     res.status(200).json(clinic);
   } catch (error) {
     res.status(500).json({ error: error.message });
